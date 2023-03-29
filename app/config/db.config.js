d2f706e1e68a38b240209128781a7e8cf2eb2a26
samuelsormin <christlegacies@gmail.com>
2023-03-30 06:42:27 +0700

    update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,9 +1,12 @@
+const mysql2 = require("mysql2")
+
 module.exports = {
     HOST: process.env.DB_HOST,
     USER: process.env.DB_USER,
     PASSWORD: process.env.DB_PASSWORD,
     DB: process.env.DB_NAME,
     dialect: "mysql",
+    dialectModule: mysql2,
     pool: {
         max: 5,
         min: 0,
